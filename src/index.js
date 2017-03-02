2cdcd3d348c8e840b825d1a5d36ca02a3f3719eb
Donno <Donn.LaBonte@gmail.com>
2017-03-01 22:20:04 -0600

    DL - section 1 update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyC3HhBXJkHttX_k09ToGRP5432dWNjYA3s';
 
 // Create a new component. This component should produce some html
 
@@ -9,7 +12,11 @@ import ReactDOM from 'react-dom';
 
 //const App = function(){
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 };
 
 
